a4fc3b5c4b7cc699190974dc2a41b7c338384e4b
Alex Andreiev <darkwasabj@gmail.com>
2023-04-27 19:53:34 +0300

    add config comment

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ return [
     'api_uri' => env('TAXES_MICROSERVICE_URL', 'https://dev.taxes.jauntin.com'),
     'cache'   => [
         'driver' => env('CACHE_DRIVER', 'file'),
-        'ttl'    => env('CACHE_TTL'),
+        'ttl'    => env('CACHE_TTL', 0), // cache ttl in seconds, use 0 or -1 to disable cache
     ],
 ];
