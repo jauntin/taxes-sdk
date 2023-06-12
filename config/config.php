2c2862702ba0a62175bc0543bb69216d9251a86f
Alex Andreiev <darkwasabj@gmail.com>
2023-06-12 18:58:20 +0300

    Add fallback env variable

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ return [
     'api_uri' => env('TAXES_MICROSERVICE_URL', 'https://dev.taxes.jauntin.com'),
     'cache'   => [
         'driver' => env('TAXES_CACHE_DRIVER', env('CACHE_DRIVER', 'file')),
-        'ttl'    => env('TAXES_CACHE_TTL', -1), // cache ttl in seconds, use 0 to cache forever or -1 to disable cache
+        'ttl'    => env('TAXES_CACHE_TTL', env('CACHE_TTL', -1)), // cache ttl in seconds, use 0 to cache forever or -1 to disable cache
     ],
 ];
