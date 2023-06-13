df1075895122f79df8ae950ea97a304dfbc8aa44
Alex Andreiev <darkwasabj@gmail.com>
2023-06-13 11:55:14 +0300

    GBLIE-103: add tax filters

--- src/Query/CalculateQuery.php
+++ src/Query/CalculateQuery.php
@@ -50,6 +50,30 @@ class CalculateQuery
     }
 
     /**
+     * @param array|string[] $codes
+     *
+     * @return $this
+     */
+    public function include(array $codes): self
+    {
+        $this->params['include'] = $codes;
+
+        return $this;
+    }
+
+    /**
+     * @param array|string[] $codes
+     *
+     * @return $this
+     */
+    public function exclude(array $codes): self
+    {
+        $this->params['exclude'] = $codes;
+
+        return $this;
+    }
+
+    /**
      * @param string $municipalCode
      *
      * @return $this
@@ -88,6 +112,8 @@ class CalculateQuery
             'state'         => ['required', 'string'],
             'amount'        => ['required', 'int', 'min:1'],
             'municipalCode' => [$requiredWithMunicipal, 'string'],
+            'codes'         => ['array'],
+            'codes.*'       => ['string'],
         ]);
     }
 }
