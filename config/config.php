687002011b159210bb3658b76658c2765e3a99a3
Sid Momin <sid.momin@mmkkyweb.com>
2023-07-06 19:42:38 +0000

    Removes default tax server

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_uri' => env('TAXES_MICROSERVICE_URL', 'https://dev.taxes.jauntin.com'),
+    'api_uri' => env('TAXES_MICROSERVICE_URL'),
     'cache'   => [
         'driver' => env('TAXES_CACHE_DRIVER', env('CACHE_DRIVER', 'file')),
         'ttl'    => env('TAXES_CACHE_TTL', env('CACHE_TTL', -1)), // cache ttl in seconds, use 0 to cache forever or -1 to disable cache
